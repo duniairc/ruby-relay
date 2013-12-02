13295e2c678cd5a39493097e34a8cc0d8dd5c501
Andrew Northall <andy@somasonic.org>
2013-12-02 17:58:44 +0000

    Fix a few bugs and add mode relaying

--- lib/relay.rb
+++ lib/relay.rb
@@ -15,11 +15,14 @@ class RelayPlugin
   listen_to :leaving, method: :relay_part
   listen_to :join, method: :relay_join
   listen_to :nick, method: :relay_nick
+  listen_to :mode, method: :relay_mode
   
   
   def relay(m)
     return if m.user.nick == @bot.nick
-    network = Format(:bold, "[#{@bot.irc.network.name}]")
+    netname = @bot.irc.network.name.to_s
+    return unless m.channel == "#" + $config["servers"][netname]["channel"]
+    network = Format(:bold, "[#{netname}]")
     if m.action?
       message = "#{network} * #{m.user.nick} #{m.action_message}"
     else
@@ -27,10 +30,27 @@ class RelayPlugin
     end
     send_relay(message)
   end
-
+  
+  def relay_mode(m)
+    return if m.params.nil?
+    return if @bot.irc.network.name.nil? #not connected yet
+    netname = @bot.irc.network.name.to_s
+    return unless m.params[0] == "#" + $config["servers"][netname]["channel"]
+    if m.user.nil?
+      user = m.raw.split(":")[1].split[0]
+    else
+      user = "#{m.user.nick} (#{m.user.mask.to_s.split("!")[1]})"
+    end
+    network = Format(:bold, "[#{@bot.irc.network.name}]")
+    message = "#{network} - #{user} set mode #{m.params[1..-1].join(" ")} on #{m.params[0]}."
+    send_relay(message)
+  end
+  
   def relay_nick(m)
     return if m.user.nick == @bot.nick
-    network = Format(:bold, "[#{@bot.irc.network.name}]")
+    netname = @bot.irc.network.name.to_s
+    return unless m.user.channels.include? "#" + $config["servers"][netname]["channel"]
+    network = Format(:bold, "[#{netname}]")
     message = "#{network} - #{m.user.last_nick} (#{m.user.mask.to_s.split("!")[1]}) " + \
               "is now known as #{m.user.nick}."
     send_relay(message)
@@ -38,11 +58,13 @@ class RelayPlugin
   
   def relay_part(m, actionuser = nil)
     return if m.user.nick == @bot.nick
-    network = Format(:bold, "[#{@bot.irc.network.name}]")
+    netname = @bot.irc.network.name.to_s
+    return unless m.channel == "#" + $config["servers"][netname]["channel"]
+    network = Format(:bold, "[#{netname}]")
     if m.command == "PART"
       action = "parted the channel (#{m.message})"
     elsif m.command == "QUIT"
-      action = "quit"
+      action = "quit (#{m.message})"
     else
       action = "parted the channel"
     end
@@ -53,7 +75,9 @@ class RelayPlugin
   
   def relay_join(m)
     return if m.user.nick == @bot.nick
-    network = Format(:bold, "[#{@bot.irc.network.name}]")
+    netname = @bot.irc.network.name.to_s
+    return unless m.channel == "#" + $config["servers"][netname]["channel"]
+    network = Format(:bold, "[#{netname}]")
     message = "#{network} - #{m.user.nick} (#{m.user.mask.to_s.split("!")[1]}) " + \
               "has joined the channel."
     send_relay(message)
